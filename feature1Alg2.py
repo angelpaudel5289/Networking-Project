691a2aa880ab16d1ba90559da9ce08910c721fc6
shishirbhamidipati <shishir.bhamidipati@gmail.com>
2025-12-07 23:19:21 -0500

    feature 1 alg 2 port change

--- feature1Alg2.py
+++ feature1Alg2.py
@@ -1,39 +1,31 @@
-import socket
+from socket import *
 import threading
-
 server_ip = "127.0.0.1"
-server_port = 5555  # Must match server
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_port = 5555
+client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((server_ip, server_port))
 
-print("Connected to server:", (server_ip, server_port))
-print("Your address:", client_socket.getsockname())
+print("Connected to server: ", (server_ip, server_port) )
+print("Your address: ", client_socket.getsockname())
 
-def receive_messages():
-    """Handles receiving messages from the server in the background."""
-    while True:
+def receive_msgs():
+    while True: 
         try:
-            data = client_socket.recv(1024)
+            data = client_socket.recv(200)
             if not data:
                 print("Server disconnected.")
                 break
             print(data.decode())
         except:
             break
-
-# Start background thread
-thread = threading.Thread(target=receive_messages)
-thread.daemon = True
+thread = threading.Thread(target=receive_msgs)
 thread.start()
 
-# Main thread sends user messages
 while True:
+    msg = input("")
     try:
-        msg = input("")
         client_socket.send(msg.encode())
     except:
         print("Connection lost.")
         break
-
 client_socket.close()
